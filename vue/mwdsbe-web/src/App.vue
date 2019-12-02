74655f9d13b238366ba45ad3945b40dbe5c61854
Dabin Lee <dabinlee0918@gmail.com>
2019-12-02 17:13:12 -0500

    2019/12/2
    interactive web using leaflet

--- vue/mwdsbe-web/src/App.vue
+++ vue/mwdsbe-web/src/App.vue
@@ -1,18 +1,25 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="MWDSBE"/>
+    <Map />
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Map from './components/Map.vue';
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
-  }
+    HelloWorld,
+    Map
+  },
+  // data() {
+  //   return {
+  //     map: null
+  //   }
+  // }
 }
 </script>
 
@@ -21,7 +28,6 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
 }
---
+++ vue/mwdsbe-web/src/components/Map.vue
@@ -0,0 +1,51 @@
+<template>
+    <div id="map"></div>
+</template>
+
+<script>
+import L from "leaflet";
+
+export default {
+    name: "Map",
+    data() {
+        return {
+            map: null
+        }
+    },
+    mounted() {
+	var map = L.map("map").fitBounds([
+			[39.86747186, -75.28030675],
+			[40.13793484, -74.95574856]
+	]);
+		
+	L.tileLayer(
+		'https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}{r}@2x.png',
+		{
+			attribution:
+				'&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+			subdomains: 'abcd',
+			maxZoom: 18
+		}
+	).addTo(map);
+
+	var layer = L.marker([39.940278, -75.153351]).addTo(map);
+	layer.bindTooltip('<b>Hello world!</b><br>I am a popup.');
+
+	// function onMapClick() {
+	// 	L.popup()
+	// 		.setLatLng(e.latlng)
+	// 		.setContent('<b>Location: </b>' + e.latlng.toString())
+	// 		.openOn(mymap);
+	// }
+
+	// map.on('click', onMapClick);
+    }
+}
+</script>
+
+<style scoped>
+	#map {
+		width: 600px;
+		height: 400px;
+	}
+</style>
